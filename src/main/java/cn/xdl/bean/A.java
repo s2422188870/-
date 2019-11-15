3310cc96bd5498bd0670fd619b43515237f0ee5d
s2422188870 <2422188870@qq.com>
2019-11-15 17:18:09 +0800

    hhhh

--- src/main/java/cn/xdl/bean/A.java
+++ src/main/java/cn/xdl/bean/A.java
@@ -2,6 +2,6 @@ package src.main.java.cn.xdl.bean;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("hello word");
+        System.out.println("hello word1");
     }
 }
